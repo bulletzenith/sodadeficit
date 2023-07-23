5dc3ba8c5e4a9ca78c4bc06a906462aa20b2bf7d
bulletzenith <hexagon.tr0@gmail.com>
2023-07-23 19:31:19 +0300

    Rotate Player

--- Assets/Scripts/PlayerLook.cs
+++ Assets/Scripts/PlayerLook.cs
@@ -27,5 +27,10 @@ public class PlayerLook : MonoBehaviour
 
         yRotation += mouseX;
         xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        // rotate camera and player 
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
     }
 }
